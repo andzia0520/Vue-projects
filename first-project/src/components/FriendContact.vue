fa7560a1aae5c5a31fd52b84d012c6751a32f477
andzia0520 <ankadaniluk@gmail.com>
2023-10-17 14:36:02 +0200

    new components

--- first-project/src/components/FriendContact.vue
+++ first-project/src/components/FriendContact.vue
@@ -3,11 +3,13 @@
 		<h2>{{ name }} {{ isFavorite ? '(favorite)' : '' }}</h2>
 		<button @click="toggleFavorite">Toggle favorite</button>
 		<button @click="toggleDetails">{{ detailsVisible ? 'Hide' : 'Show' }} Details</button>
+		<button @click="deleteData">Delete </button>
 		<ul v-if="detailsVisible">
 			<li><strong>Phone: </strong> {{ phoneNumber }}</li>
 			<li><strong>Email: </strong> {{ email }}</li>
 		</ul>
 	</li>
+	
 </template>
 
 <script>
@@ -35,6 +37,18 @@ export default {
 			default: false,
 		},
 	},
+	emits: ['toggle-favorite', 'delete-contact'],
+
+	// emits: {
+	// 	'toggle-favorite': function (id) {
+	// 		if (id) {
+	// 			return true;
+	// 		} else {
+	// 			return false;
+	// 		}
+	// 	},
+	// },
+	
 	data() {
 		return {
 			detailsVisible: false,
@@ -47,6 +61,10 @@ export default {
 
 		toggleFavorite() {
 			this.$emit('toggle-favorite', this.id);
+		},
+
+		deleteData() {
+			this.$emit('delete-contact', this.id);
 		},
 	},
 };
